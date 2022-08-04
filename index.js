139225d8bdb572c8578b7fefcf7a8daaa6bfa7dc
ad3rinto <aad3rinto@gmail.com>
2022-08-04 12:01:28 +0100

    add event listeener

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 $('h1').css('color', 'red')
+
+// Adding event listeners
+
+$('h1').click(function(){
+   $('h1').css('color', 'black'); 
+})
+
+
+$('button').click(function(){
+    console.log('I got clicked');
+})
